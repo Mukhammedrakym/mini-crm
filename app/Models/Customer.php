7666159f27fc96330fd0e55b8c229633029041ce
mukhammedrakym <mmm-workspace@ya.ru>
2026-02-08 22:35:06 +0500

    customers, tickets models

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Ticket;
 
 class Customer extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'phone',
+        'email',
+    ];
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Customer;
 
 class Ticket extends Model
 {
-    //
+    protected $fillable = [
+        'customer_id',
+        'subject',
+        'message',
+        'status',
+        'answered_at',
+    ];
+
+    protected $casts = [
+        'answered_at' => 'datetime',
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
 }
